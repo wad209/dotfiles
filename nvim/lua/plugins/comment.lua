e6f63f1e3759dfa7d8e2c5b3367724f124460208
Wade Duvall <ackthet@gmail.com>
2023-11-17 15:23:05 -0500

    style: move Comment.nvim and and nvim-cmp to more betterer lazy spec.

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,18 +1,19 @@
-local M = {
+return {
   "numToStr/Comment.nvim",
-  event = {"BufRead", "BufNewFile"},
+  event = { "BufRead", "BufNewFile" },
   dependencies = {
     {
       "JoosepAlviste/nvim-ts-context-commentstring",
-      event = "VeryLazy",
+      lazy = true,
+      opts = {
+        enable_autocmd = false,
+      },
     },
   },
+  opts = function()
+    local opts = {
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    }
+    return opts
+  end,
 }
-
-function M.config()
-  require("Comment").setup{
-    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-  }
-end
-
-return M
