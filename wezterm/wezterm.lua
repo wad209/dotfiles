43a2132f3549847a0b2b1ec81db55628a3a59a1d
Wade S Duvall <ackthet@gmail.com>
2023-12-18 17:58:21 -0500

    Feat: wezterm config with onedark-warmer colors.

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+local config = {
+  font = wezterm.font("JetBrains Mono"),
+  audible_bell = "Disabled",
+  -- color_scheme = "One Dark (Gogh)",
+  color_scheme = "onedark",
+  font_size = 11,
+  use_fancy_tab_bar = false,
+}
+return config
